18c93aeaf68dc7df02d7e6c8f85af37dc136f0fd
Amirhosseinsedghi <amirhossein.sedghi1996@gmail.com>
2024-09-24 12:04:44 -0700

    MSE not good

--- test.py
+++ test.py
@@ -5,11 +5,15 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import StandardScaler
+from sklearn.feature_selection import SelectKBest, f_regression
+
 
 #import file
 data= pd.read_csv('D:\education\ML\linear regression\linear-regression.test\Housing.csv')
 print(type(data)) 
 print(data.columns)
+
 #cleaning data
 print(data.info())
 print(data.describe())
@@ -17,11 +21,17 @@ print(data.head())
 data= data.dropna()
 
 
-X= data [['bedrooms','bathrooms']] #featurs
+X= data [['bedrooms','bathrooms','sqft_living','sqft_lot','floors','waterfront','view','condition','grade','sqft_above','sqft_basement','yr_built','yr_renovated','zipcode','lat','long','sqft_living15','sqft_lot15']] #featurs
 y= data ['price'] # dependent variable
 
+scaler= StandardScaler()
+X_scaled= scaler.fit_transform(X)
+
+
+
 #seprating data into training and testing
-X_train, X_test, y_train, y_test= train_test_split(X, y,test_size= 0.3, random_state=42)
+X_train, X_test, y_train, y_test= train_test_split(X_scaled, y,test_size= 0.3, random_state=42)
+
 
 #making regression model
 model=LinearRegression()
